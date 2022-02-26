c909dfad8e1d7de952335735ecfda7b972e92ae8
Ilham Bahtiar <bahtiari073@gmail.com>
2022-02-26 22:25:51 +0800

    fix typo

--- lua/ryn/plugins/alpha.lua
+++ lua/ryn/plugins/alpha.lua
@@ -15,7 +15,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "冷  Find project", ":Telescope projects <CR>"),
+	dashboard.button("p", "冷 Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "ﭨ  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("s", "  Last Session", ":lua require('persistence').load({ last = true })<CR>"),
