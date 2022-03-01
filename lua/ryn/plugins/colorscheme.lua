63c2f776c3689dce16dc4bd0ae53bc9bde63aa2a
Ilham Bahtiar <bahtiari073@gmail.com>
2022-03-01 21:50:20 +0800

    change colorscheme configs

--- lua/ryn/plugins/colorscheme.lua
+++ lua/ryn/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 require("rynkai").setup {
 	theme = "catppuccin", -- this field is a must.
-	config_file = "lua/ryn/plugins/colorscheme.lua", -- this field is a must.
-	rynkai_dir = "/site/pack/packer/opt/rynkai.nvim/lua/rynkai/colors/", -- this field is a must.
+	config_file = vim.fn.stdpath "config" .. "/lua/ryn/plugins/colorscheme.lua", -- this field is a must.
+	rynkai_dir = vim.fn.stdpath "data" .. "/site/pack/packer/opt/rynkai.nvim/lua/rynkai/colors/", -- this field is a must.
 }
 
 vim.cmd [[ colorscheme rynkai ]]
