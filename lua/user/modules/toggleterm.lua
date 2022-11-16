153cc0a437aca10a43d0cb3fb80334d380f25991
Ilham Bahtiar <bahtiari073@gmail.com>
2022-11-17 06:26:02 +0800

    refactor

--- lua/user/modules/toggleterm.lua
+++ lua/user/modules/toggleterm.lua
@@ -1,70 +1,38 @@
-local util = require"user.util"
+local toggleterm = require "toggleterm"
 
-local present, toggleterm = pcall(require, "toggleterm")
-if not present then
-  return
-end
-local configs = {
-  escape_map = "<C-\\>",
-  setup = {
-    open_mapping = [[<C-t>]],
-    -- direction = "float",
-    float_opts = {
-      border = "curved",
-      winblend = 5,
-      highlights = {
-        border = "NormalFloat",
-        background = "NormalFloat",
-      },
-    },
+toggleterm.setup {
+  open_mapping = [[<C-t>]],
+  -- direction = "float",
+  highlights = {
+    Normal = { link = "Normal" },
+    NormalFloat = { link = "NormalFloat" },
+    FloatBorder = { link = "FloatBorder" },
+    SignColumn = { link = "Normal" },
+  },
+  float_opts = {
+    border = "curved",
+    winblend = 5,
+    width = math.min(vim.o.columns, math.max(80, vim.o.columns - 10)),
+    height = math.min(vim.o.lines, math.max(20, vim.o.lines - 6)),
   },
 }
 
-toggleterm.setup(configs.setup)
-
-local escape_map = configs.escape_map
+-- set autocmd for escape keymap from terminal buffer
+local escape_map = "<C-\\>"
 function _G.set_terminal_keymaps()
   vim.api.nvim_buf_set_keymap(0, "t", escape_map, [[<C-\><C-n><C-W>l]], { noremap = true })
 end
-
 vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
 
-local Terminal = require("toggleterm.terminal").Terminal
-
-local function setup(cmd)
-  if not cmd then
-    return
-  end
-
-  local config = {
+-- Create custom terminal
+local function custom_terminal(cmd)
+  if not cmd then return end
+  return require("toggleterm.terminal").Terminal:new {
     cmd = cmd,
-		hidden = true,
-    dir = "git_dir",
-    direction = "window",
-    float_opts = {
-      border = "double",
-    },
-    -- function to run on opening the terminal
-    on_open = function(term)
-      vim.cmd "startinsert!"
-      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-    end,
-    -- -- function to run on closing the terminal
-    -- on_close = function(term)
-    --   vim.cmd "Closing terminal"
-    -- end,
+    -- hidden = true,
+    direction = "float",
   }
-
-	return Terminal:new(config)
 end
 
-function __LAZYGIT_TOGGLE()
-  setup("lazygit"):toggle()
-end
-
-function __WUZZ_TOGGLE()
-  setup("wuzz"):toggle()
-end
-
-util.keymap("n", "<F6>", ":lua __LAZYGIT_TOGGLE()<CR>")
-util.keymap("n", "<F7>", ":lua __WUZZ_TOGGLE()<CR>")
+vim.keymap.set("n", "<F6>", function() custom_terminal("lazygit"):toggle() end) -- lazygit
+vim.keymap.set("n", "<F7>", function() custom_terminal("wuzz"):toggle() end) -- wuzz
