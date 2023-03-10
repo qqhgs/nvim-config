a48f0b6ca706c011d0941fa306c10edabce059ef
Ilham Bahtiar <bahtiari073@gmail.com>
2023-03-11 06:31:48 +0800

    feat(keymap): add tab movement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -128,3 +128,11 @@ map(
   function() Util.toggle("conceallevel", false, { 0, conceallevel }) end,
   { desc = "Toggle Conceal" }
 )
+
+-- tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
