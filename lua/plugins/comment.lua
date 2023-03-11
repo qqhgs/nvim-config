b9899eed3b4ec4b12a4a2aba47bfe4158ab38b65
Ilham Bahtiar <bahtiari073@gmail.com>
2023-03-11 08:09:45 +0800

    fix(comment): fix keys not working when plugin unavailable

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,19 +2,23 @@ return {
   { "JoosepAlviste/nvim-ts-context-commentstring" },
   {
     "numToStr/Comment.nvim",
-    -- event = "VeryLazy",
     opts = {
       ignore = "^$",
       hooks = {
         pre = function() require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook() end,
       },
     },
-    keys = function()
-      local api = require("Comment.api")
-      return {
-        { "<C-_>", api.call("toggle.linewise.current", "g@$"), mode = "n", expr = true },
-        { "<C-_>", api.call("toggle.linewise", "g@$"), mode = "x", expr = true },
-      }
-    end,
+    keys = {
+      {
+        "<C-_>",
+        function()
+          return vim.v.count == 0 and "<Plug>(comment_toggle_linewise_current)"
+            or "<Plug>(comment_toggle_linewise_count)"
+        end,
+        mode = "n",
+        expr = true,
+      },
+      { "<C-_>", "<Plug>(comment_toggle_linewise_visual)", mode = "x" },
+    },
   },
 }
