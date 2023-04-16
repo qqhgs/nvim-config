4f042e95237ac54822a36100bb7497d63bbba506
Ilham Bahtiar <bahtiari073@gmail.com>
2023-04-16 22:10:41 +0800

    feat(completion): change cmp behaviour

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,12 +10,42 @@ return {
   },
   opts = function()
     local cmp = require("cmp")
+    local snip_status_ok, luasnip = pcall(require, "luasnip")
+    if not snip_status_ok then return end
+
+    local function has_words_before()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
+    local border_opts = {
+      border = "single",
+      winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+    }
+
     return {
+      preselect = cmp.PreselectMode.None,
       completion = {
         completeopt = "menu,menuone,noinsert",
+        scrollbar = false,
       },
       snippet = {
-        expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        expand = function(args) luasnip.lsp_expand(args.body) end,
+      },
+      confirm_opts = {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = false,
+      },
+      window = {
+        completion = cmp.config.window.bordered(border_opts),
+        documentation = cmp.config.window.bordered(border_opts),
+      },
+      duplicates = {
+        nvim_lsp = 1,
+        luasnip = 1,
+        cmp_tabnine = 1,
+        buffer = 1,
+        path = 1,
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -25,6 +55,26 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       formatting = {
         format = function(_, vim_item)
@@ -43,16 +93,22 @@ return {
           end
 
           vim_item.menu = vim_item.kind
-          vim_item.kind = kind[vim_item.kind]
+          vim_item.kind = string.format("%s", kind[vim_item.kind])
           return vim_item
         end,
         fields = { "kind", "abbr", "menu" },
       },
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        {
+          name = "buffer",
+          option = {
+            get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+          },
+          priority = 500,
+        },
+        { name = "path", priority = 250 },
       }),
       experimental = {
         ghost_text = {
