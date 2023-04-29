a650c4205030391442d84e96e4f150ba452aed1a
Ilham Bahtiar <bahtiari073@gmail.com>
2023-04-30 01:03:30 +0800

    feat: add split-join plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -42,9 +42,43 @@ return {
     event = "VeryLazy",
     opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
     keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore Session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore Last Session",
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Don't Save Current Session",
+      },
     },
+  },
+
+  {
+    "Wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = {
+      { "gM", "<cmd>TSJToggle<cr>", desc = "Toggle node under cursor" },
+      { "gS", "<cmd>TSJSplit<cr>", desc = "Split node under cursor" },
+      { "gJ", "<cmd>TSJJoin<cr>", desc = "Join node under cursor" },
+    },
+    config = function()
+      require("treesj").setup({
+        use_default_keymaps = false,
+        max_join_length = 1000,
+      })
+    end,
   },
 }
