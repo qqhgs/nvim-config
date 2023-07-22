c5ec15162d93bd47a46a19efd9d7eab3366e9af4
Ilham Bahtiar <bahtiari073@gmail.com>
2023-07-22 23:54:06 +0800

    feat: add rainbow-delimiter plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,7 +3,10 @@ return {
   {
     "j-hui/fidget.nvim",
     event = "VeryLazy",
-    config = function() require("fidget").setup() end,
+    branch = "legacy",
+    config = function()
+      require("fidget").setup()
+    end,
   },
 
   -- better vim.ui
@@ -20,6 +23,35 @@ return {
         require("lazy").load({ plugins = { "dressing.nvim" } })
         return vim.ui.input(...)
       end
+    end,
+  },
+
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    event = "VeryLazy",
+    config = function()
+      -- This module contains a number of default definitions
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
     end,
   },
 }
